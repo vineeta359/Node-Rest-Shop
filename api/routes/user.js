508324fcf89a728e253b51a193e6238f4aa07f48
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2022-06-30 16:39:52 +0530

    added user login and jwt sign in

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt"); //for securing passwords
-
+const jwt = require("jsonwebtoken"); //to send the token
 const User = require("../models/user");
 
 router.post("/signup", (req, res, next) => {
@@ -39,6 +40,49 @@ router.post("/signup", (req, res, next) => {
           }
         });
       }
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      console.log(user);
+      if (!user)
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (err)
+          return res.status(401).json({
+            message: "Auth failed",
+          });
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user.email,
+              userId: user._id,
+            },
+            process.env.JWT_SECRET_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed",
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
     });
 });
 
